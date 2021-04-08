9c4842f08c94a7926a12ead6fd93916e1bf411d6
AlexAbramoff <56651177+AlexAbramoff@users.noreply.github.com>
2021-04-08 22:28:38 +0300

    Add files via upload

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -10,14 +10,13 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  const arr1 = s1.split('').sort();
-  const arr2 = s2.split('').sort();
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
   let countOfRepeat = 0;
   for (let i = 0; i < arr1.length; i++) {
     if (arr2.includes(arr1[i])) {
       countOfRepeat++;
       delete arr2[arr2.indexOf(arr1[i])];
-      delete arr1[i];
     }
   }
   return countOfRepeat;

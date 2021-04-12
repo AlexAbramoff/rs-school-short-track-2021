c74565403ed8d56517e6477298e1e468a07edd62
AlexAbramoff <56651177+AlexAbramoff@users.noreply.github.com>
2021-04-12 20:27:04 +0300

    feat: add task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const encodeArr = [];
+  let countChar = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      countChar++;
+    } else {
+      if (countChar === 1) {
+        encodeArr.push(str[i]);
+      } else {
+        encodeArr.push(countChar + str[i]);
+      }
+      countChar = 1;
+    }
+  }
+  return encodeArr.join('');
 }
 
 module.exports = encodeLine;
